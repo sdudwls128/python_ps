5becdc0728432f7d7df42a975b8e92c5a945ccf2
sdudwls128 <sdudwls128@naver.com>
2021-07-18 20:29:48 +0900

    success

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ y = 1
 d[x][y] = 9
 
 while x < 10 and y < 10:
-    if d[x][y] == 2:
-        d[x][y] = 9
-        break
     if d[x][y+1] != 1:
         y += 1
     elif d[x+1][y] != 1:
         x += 1
     else:
+        break
+    if d[x][y] == 2:
+        d[x][y] = 9
         break
     d[x][y] = 9
 
