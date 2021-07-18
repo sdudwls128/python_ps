bdcb46a24c1d01385cdcda6910a58dbb204dab83
sdudwls128 <sdudwls128@naver.com>
2021-07-18 20:29:10 +0900

    test commit

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
-d = [[]*10 for _ range(10)]
+d = [[0]*10 for _ in range(10)]
 for i in range(10):
     d[i] = list(map(int, input().split()))
-
 x = 1
 y = 1
+d[x][y] = 9
 
-while 1:
-    d[x][y] = 9
-    if d[x][y+1] == 1:
-        if d[x+1][y] == 1:
-            break
-        else:
-            x += 1
-        y += 1
-    elif d[x][y+1] == 1:
-        if d[x+1][y] == 0:
-            x += 1
-        elif d[x+1][y] == 1:
-            break
+while x < 10 and y < 10:
     if d[x][y] == 2:
         d[x][y] = 9
         break
-    if x==9 and y==9:
-        d[x][y] = 9
+    if d[x][y+1] != 1:
+        y += 1
+    elif d[x+1][y] != 1:
+        x += 1
+    else:
         break
+    d[x][y] = 9
+
+for i in range(10):
+    for j in range(10):
+        print(d[i][j], end=' ')
+    print()
